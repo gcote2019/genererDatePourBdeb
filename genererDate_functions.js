31aa80b791ca6e8743331b7161b2158945faafbd
gcote2019 <46534528+gcote2019@users.noreply.github.com>
2023-08-16 12:13:10 -0400

    Clean-up
    Generate checkboxes using js
    better use of bit wise
    use date.valueOf() because == doesn't work

--- genererDate_functions.js
+++ genererDate_functions.js
@@ -26,71 +26,82 @@ const Mois = {
   Decembre : 11
 }
 
-const JoursBitwise = {
-  DimancheBitwise: 0,
-  LundiBitwise: 0x01,
-  MardiBitwise : 0x02,
-  MercrediBitwise: 0x04,
-  JeudiBitwise: 0x08,
-  VendrediBitwise: 0x10,
-  SamediBitwise: 0
-}
-
-
 const tableau = [
-  {
-    "jour" : "Lundi",
-    "valeur": JoursBitwise.LundiBitwise
-  },
-  {
-    "jour" : "Mardi",
-    "valeur": JoursBitwise.MardiBitwise
-  },
-  {
-    "jour" : "Mercredi",
-    "valeur": JoursBitwise.MercrediBitwise
-  },
-  {
-    "jour" : "Jeudi",
-    "valeur": JoursBitwise.JeudiBitwise
-  },
-  {
-    "jour" : "Vendredi",
-    "valeur": JoursBitwise.VendrediBitwise
-  }
+  "Lundi",
+  "Mardi",
+  "Mercredi",
+  "Jeudi",
+  "Vendredi"
 ];
 
 // javascript janvier est 0 et decembre est 11
 let conges = [
-  new Date(2023, Mois.Septembre, 4),
-  new Date(2023, Mois.Septembre, 19),
+  new Date(2023, Mois.Septembre, 4).valueOf(),
+  new Date(2023, Mois.Septembre, 19).valueOf(),
 
-  new Date(2023, Mois.Octobre, 9),
-  new Date(2023, Mois.Octobre, 19),
+  new Date(2023, Mois.Octobre, 9).valueOf(),
+  new Date(2023, Mois.Octobre, 19).valueOf(),
 
-  new Date(2023, Mois.Novembre, 10),
+  new Date(2023, Mois.Novembre, 10).valueOf(),
 
-  new Date(2023, Mois.Decembre, 4),
+  new Date(2023, Mois.Decembre, 4).valueOf(),
 ];
 
 let exceptions = [
   {"date" : new Date(2023, Mois.Octobre, 11),
-  "jour" : JoursBitwise.LundiBitwise
+  "jour" : Jours.Lundi
   }
 ];
 
 let debut = new Date(2023, Mois.Aout, 21);
-let fin = new Date(2023, Mois.Decembre, 12);
+let fin = new Date(2023, Mois.Decembre, 12).valueOf();
 
+function creerCheckboxes() {
+  let div = document.getElementById("checkboxes");
+             
+  tableau.forEach(element => {
+    // creating checkbox element
+    let checkbox = document.createElement('input');
+    
+      // Assigning the attributes
+      // to created checkbox
+    checkbox.type = "checkbox";
+    checkbox.name = element;
+    checkbox.value = element;
+    checkbox.id = element;
+      
+    // creating label for checkbox
+    var label = document.createElement('label');
+      
+    // assigning attributes for
+    // the created label tag
+    label.htmlFor = element;
+      
+    // appending the created text to
+    // the created label tag
+    label.appendChild(document.createTextNode(" " + element));
+      
+    // appending the checkbox
+    // and label to div
+    div.appendChild(checkbox);
+    div.appendChild(label);
+    let p = document.createElement('p');
+    p.textContent = '';
+    div.appendChild(p);
+  });
+
+}
 function verifierCheckboxes() {
   jours = 0;
+  let valeur = 1;
   tableau.forEach(element => {
     try {
-      let checkbox = document.getElementById(element.jour);
+      let checkbox = document.getElementById(element);
 
       if (checkbox.checked) {
-        ajouterJour(element.valeur);
+        ajouterJour(valeur);
       }
+      valeur <<= 1;
     } catch (error) {
       console.log("Pas trouvé");
     }
@@ -105,23 +116,11 @@ function enleverJour(jour) {
   jours &= ~jour;
 }
 
-// c'est vraiment bizarre. Je ne trouve pas mon erreur mais date1 == date2 ne fonctionne pas
-function compareDate(date1, date2) {
-  let diff = date1.getYear() > date2.getYear();
-  if (diff == 0) {
-    diff = date1.getMonth() - date2.getMonth()
-  }
-  if (diff == 0) {
-    diff = date1.getDate() - date2.getDate()
-  }
-  return diff;
-}
-
 function verifierConge(jourCourant) {
   let trouve = false;
   for (let i = 0; i < conges.length && !trouve; i++) {
     // pourquoi conges[i] == jourCourant me retourne false même quand c'est égal? 
-    if (compareDate(jourCourant, conges[i]) == 0) {
+    if (jourCourant.valueOf() == conges[i]) {
       trouve = true;
     }
   }
@@ -131,30 +130,29 @@ function verifierConge(jourCourant) {
 function obtenirJourDeLaSemaine(jourCourant) {
   let valeur = 0;
   for (let i = 0; i < exceptions.length && (valeur == 0); i++) {
-    if (compareDate(exceptions[i].date, jourCourant) == 0) {
-      valeur = exceptions[i].jour;
+    if (jourCourant.valueOf() == exceptions[i].date) {
+      valeur = 1 << (exceptions[i].jour - 1);
     }
   }
   if (valeur == 0) {
     try {
-      valeur = tableau[jourCourant.getDay() - 1].valeur;
+      valeur = 1 << (jourCourant.getDay() - 1);
     } catch (error) {
     }
   }
   return valeur;
 }
 function genererDate() {
-  console.log("genererDate");
   verifierCheckboxes();
-  console.log(jours);
+
   try {
     let container = document.getElementById("resultats");
     let resultats = "";
     if (jours != 0) {
-      let jourCourant = debut;
+      let jourCourant = new Date(debut);
       console.log(jourCourant.toISOString().split('T')[0]);
       //while (jourCourant < fin) {
-      while (compareDate(jourCourant, fin) < 0) {
+      while (jourCourant.valueOf() < fin) {
         while (jourCourant.getDay() == Jours.Dimanche || jourCourant.getDay() == Jours.Samedi || verifierConge(jourCourant)) {
           jourCourant.setDate(jourCourant.getDate() + 1);
         }
